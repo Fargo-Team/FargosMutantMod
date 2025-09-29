e21c053a82c516b40682ac9acf97524899418952
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2025-09-28 23:49:00 -0700

    Added achievements for crafting the Multitask Center, Elemental Assembler, and the Crucible of The Cosmos.

---
+++ Content/Achievements/CrucibleofTheCosmosAchievement.cs
@@ -0,0 +1,25 @@
+﻿using Fargowiltas.Content.Items.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Achievements;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Content.Achievements
+{
+    public class CrucibleofTheCosmosAchievement : ModAchievement
+    {
+        public override string TextureName => "Fargowiltas/Content/Achievements/MutantAchievements";
+
+        public override int Index => 11;
+
+        public override void SetStaticDefaults()
+        {
+            Achievement.SetCategory(Terraria.Achievements.AchievementCategory.Collector);
+
+            AddItemCraftCondition(ModContent.ItemType<CrucibleCosmos>());
+        }
+    }
+}
---
+++ Content/Achievements/MultitaskCenterAchievement.cs
@@ -0,0 +1,24 @@
+﻿using Fargowiltas.Content.Items.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Content.Achievements
+{
+    public class MultitaskCenterAchievement : ModAchievement
+    {
+        public override string TextureName => "Fargowiltas/Content/Achievements/MutantAchievements";
+
+        public override int Index => 9;
+
+        public override void SetStaticDefaults()
+        {
+            Achievement.SetCategory(Terraria.Achievements.AchievementCategory.Collector);
+
+            AddItemCraftCondition(ModContent.ItemType<MultitaskCenter>());
+        }
+    }
+}

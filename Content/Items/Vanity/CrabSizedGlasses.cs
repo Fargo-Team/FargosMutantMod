d417b74b869820cc26c569768692f1096bced6af
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2026-01-21 17:59:51 -0800

    feat: Cool Crab pet now animates in the Player Select screen.

--- Content/Items/Vanity/CrabSizedGlasses.cs
+++ Content/Items/Vanity/CrabSizedGlasses.cs
@@ -17,24 +17,20 @@ namespace Fargowiltas.Content.Items.Vanity
 
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.ShadowOrb);
+            Item.DefaultToVanitypet(ModContent.ProjectileType<CoolCrab>(), ModContent.BuffType<CoolCrabBuff>());
             Item.rare = ItemRarityID.Green;
             Item.width = 20;
             Item.height = 40;
-            Item.shoot = ModContent.ProjectileType<CoolCrab>();
-            Item.buffType = ModContent.BuffType<CoolCrabBuff>();
             Item.value = Item.sellPrice(0, 0, 0, 10);
         }
 
-        public override void UseStyle(Player player, Rectangle
-            heldItemFrame)
+        public override bool? UseItem(Player player)
         {
-            base.UseStyle(player, heldItemFrame);
-
-            if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+            if (player.whoAmI == Main.myPlayer)
             {
                 player.AddBuff(Item.buffType, 3600);
             }
+            return true;
         }
     }
 }
--- Content/Projectiles/CoolCrab.cs
+++ Content/Projectiles/CoolCrab.cs
@@ -11,8 +11,26 @@ namespace Fargowiltas.Content.Projectiles
     {
         public override void SetStaticDefaults()
         {
-            Main.projFrames[Projectile.type] = 8;
-            Main.projPet[Projectile.type] = true;
+            Main.projFrames[Type] = 8;
+            Main.projPet[Type] = true;
+
+            ProjectileID.Sets.CharacterPreviewAnimations[Type] = 
+                ProjectileID.Sets.SimpleLoop(0, 8, 5)
+                .WithOffset(new(-2, -2))
+                .WithCode(CustomPreviewAnimation);
+        }
+
+        private void CustomPreviewAnimation(Projectile proj, bool walking)
+        {
+            if (walking)
+            {   
+                if (proj.frame > 6)
+                    proj.frame = 0;            
+            }
+            else
+            {
+                proj.frame = 0;
+            }
         }
 
         public override void SetDefaults()
@@ -76,7 +94,7 @@ namespace Fargowiltas.Content.Projectiles
             Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
 
             int num156 = texture2D13.Height / 8; //ypos of lower right corner of sprite to draw
-            int y3 = num156 * Frame; //ypos of upper left corner of sprite to draw
+            int y3 = num156 * (Projectile.isAPreviewDummy? Projectile.frame : Frame); //ypos of upper left corner of sprite to draw
             Rectangle rectangle = new(0, y3, texture2D13.Width, num156);
             Vector2 origin2 = rectangle.Size() / 2f;
             Vector2 drawOffset = Vector2.UnitY * 0 * Projectile.scale;

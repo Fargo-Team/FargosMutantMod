02e86b87d16cac7ee99ab888474b8a1a55fde93d
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2026-02-14 18:36:24 -0800

    feat: Resprited Core of the Frost Core, Forbidden Forbidden Fragment, and Pirate Flag.

--- Content/Items/Summons/Deviantt/ForbiddenForbiddenFragment.cs
+++ Content/Items/Summons/Deviantt/ForbiddenForbiddenFragment.cs
@@ -1,6 +1,7 @@
 using Fargowiltas.Content.Buffs.SpawnBoosters;
 using Fargowiltas.Content.Items.Summons;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,6 +11,13 @@ namespace Fargowiltas.Content.Items.Summons.Deviantt
     {
         public override int BuffType => ModContent.BuffType<ForbiddenForbiddenFragmentBuff>();
 
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(6, 5));
+            ItemID.Sets.AnimatesAsSoul[Item.type] = true;
+
+        }
     }
     public class ForbiddenForbiddenFragmentBuff : BaseSpawnBoosterBuff
     {

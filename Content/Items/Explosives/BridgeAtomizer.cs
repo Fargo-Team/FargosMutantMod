19852fe892ef93047d2949208be62998cf5920d3
JavyzTaken <jacklukas@outlook.com>
2026-04-12 01:15:48 +0200

    Added Bridge Atomizer - Deletes all platforms connected horizontally - Placeholder sprite

---
+++ Content/Items/Explosives/BridgeAtomizer.cs
@@ -0,0 +1,73 @@
+﻿using Fargowiltas.Common.Configs;
+using Fargowiltas.Common.Systems;
+using Fargowiltas.Content.Projectiles.Explosives;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Content.Items.Explosives
+{
+    public class BridgeAtomizer : ModItem
+    {
+        public override string Texture => "Fargowiltas/Content/Items/Placeholder";
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            return FargoServerConfig.Instance.InstantItems;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 10;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 10;
+            Item.height = 32;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+            Item.useAnimation = 20;
+            Item.useTime = 20;
+            Item.value = Item.buyPrice(0, 0, 3);
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType<BridgeAtomizerProj>();
+        }
+        public override bool AltFunctionUse(Player player) => true;
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.HasBuff(BuffID.NoBuilding))
+                return false;
+            Point pos = Main.MouseWorld.ToTileCoordinates();
+            if (pos.X < 0 || pos.X >= Main.maxTilesX || pos.Y < 0 || pos.Y >= Main.maxTilesY)
+                return false;
+            if (Main.tile[pos].HasTile)
+                return base.CanUseItem(player);
+            return false;
+
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 mouse = Main.MouseWorld;
+            Projectile.NewProjectile(player.GetSource_ItemUse(source.Item), mouse, Vector2.Zero, type, 0, 0, player.whoAmI, ai2: player.altFunctionUse);
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Dynamite, 1)
+                .AddRecipeGroup("Fargowiltas:AnyWoodenPlatform", 10)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Explosives/BridgeAtomizerProj.cs
@@ -0,0 +1,70 @@
+﻿using Fargowiltas.Content.Items.Tiles;
+using Fargowiltas.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Content.Projectiles.Explosives
+{
+    public class BridgeAtomizerProj : ModProjectile
+    {
+        public override string Texture => "Fargowiltas/Content/Items/Explosives/InstaBridge";
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 36;
+            Projectile.aiStyle = ProjAIStyleID.Explosive;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 1;
+        }
+
+        public override bool? CanDamage()
+        {
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            Vector2 position = Projectile.Center;
+            SoundEngine.PlaySound(SoundID.Item14, position);
+
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                return;
+            }
+            Point tileCenter = position.ToTileCoordinates();
+            int left = 0;
+            int right = 0;
+            for (int dir = -1; dir <= 1; dir += 2)
+            {
+                for (int x = 0; x < Main.maxTilesX; x++)
+                {
+                    if (x != 0)
+                    {
+                        if (dir == -1)
+                            left++;
+                        else
+                            right++;
+                    }
+                    else if (dir == 1)
+                        continue;
+                    Point pos = new(tileCenter.X + dir * x, tileCenter.Y);
+                    if (pos.X < 0 || pos.X >= Main.maxTilesX || pos.Y < 0 || pos.Y >= Main.maxTilesY)
+                        break;
+                    if (Main.tile[pos].HasTile && Main.tile[pos].TileType > TileID.Dirt && TileID.Sets.Platforms[Main.tile[pos].TileType] && FargoGlobalProjectile.OkayToDestroyTileAt(pos.X, pos.Y))
+                    {
+                        FargoGlobalTile.ClearEverything(pos.X, pos.Y, false);
+                        continue;
+                    }
+                    break;
+                }
+            }
+
+            if (Main.netMode == NetmodeID.Server)
+                NetMessage.SendTileSquare(-1, tileCenter.X - left, tileCenter.Y, left + right, 1, TileChangeType.None);
+        }
+    }
+}

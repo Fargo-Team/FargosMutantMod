503c28fb415c613ea89da4900467f765f8b50ea7
schmoovi <romandark1@gmail.com>
2026-07-09 03:50:18 -0500

    Suspicious Looking Lure and Hemoclaw Crab: Are now bait that always catches a Zombie Merman or Wandering Eye Fish, or a Hemogoblin Shark or Blood Eel, respectively.

--- Content/Items/Summons/Deviantt/HemoclawCrab.cs
+++ Content/Items/Summons/Deviantt/HemoclawCrab.cs
@@ -1,14 +1,26 @@
-using Fargowiltas.Content.Buffs;
 using Fargowiltas.Content.Items.Misc;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Fargowiltas.Content.Items.Summons.Deviantt
 {
     public class HemoclawCrab : BaseSpawnBooster
     {
-        public override int BuffType => ModContent.BuffType<HemoclawCrabBuff>();
+        public override int BuffType => 0; //irrelevant, being overriden
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.buffType = 0;
+            Item.buffTime = 0;
+            Item.useStyle = ItemUseStyleID.None;
+            Item.value /= 2;
+            Item.bait = 66;
+        }
 
         public override void AddRecipes()
         {
@@ -21,11 +33,28 @@ namespace Fargowiltas.Content.Items.Summons.Deviantt
                 .Register();
         }
     }
-    public class HemoclawCrabBuff : BaseSpawnBoosterBuff
+
+    public class HemoclawFishingPlayer : ModPlayer
     {
-        public override string Texture => "Fargowiltas/Content/Buffs/PlaceholderBuff";
-        public HemoclawCrabBuff() : base(() => [NPCID.BloodEelHead, NPCID.GoblinShark], () => Main.bloodMoon && Main.LocalPlayer.ZoneBeach, 0.2f)
+        public override bool? CanConsumeBait(Item bait) => bait.type == ModContent.ItemType<HemoclawCrab>() ? true : base.CanConsumeBait(bait);
+
+        public override void CatchFish(FishingAttempt attempt, ref int itemDrop, ref int npcSpawn, ref AdvancedPopupRequest sonar, ref Vector2 sonarPosition)
         {
+            if (Main.bloodMoon && attempt.playerFishingConditions.BaitItemType == ModContent.ItemType<HemoclawCrab>())
+            {
+                npcSpawn = Main.rand.NextBool() ? NPCID.GoblinShark : NPCID.BloodEelHead;
+                itemDrop = -1;
+            }
+        }
+
+        public override void GetFishingLevel(Item fishingRod, Item bait, ref float fishingLevel)
+        {
+            if (bait.type == ModContent.ItemType<HemoclawCrab>())
+            {
+                if (Main.bloodMoon)
+                    Player.displayedFishingInfo = Language.GetTextValue("GameUI.FishingWarning");
+                else fishingLevel = -1;
+            }
         }
     }
 }
--- Content/Items/Summons/Deviantt/SuspiciousLookingLure.cs
+++ Content/Items/Summons/Deviantt/SuspiciousLookingLure.cs
@@ -1,14 +1,26 @@
-using Fargowiltas.Content.Buffs;
 using Fargowiltas.Content.Items.Misc;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Fargowiltas.Content.Items.Summons.Deviantt
 {
     public class SuspiciousLookingLure : BaseSpawnBooster
     {
-        public override int BuffType => ModContent.BuffType<SuspiciousLookingLureBuff>();
+        public override int BuffType => 0;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.buffType = 0;
+            Item.buffTime = 0;
+            Item.useStyle = ItemUseStyleID.None;
+            Item.value /= 2;
+            Item.bait = 13;
+        }
 
         public override void AddRecipes()
         {
@@ -21,11 +33,28 @@ namespace Fargowiltas.Content.Items.Summons.Deviantt
                 .Register();
         }
     }
-    public class SuspiciousLookingLureBuff : BaseSpawnBoosterBuff
+
+    public class SuspiciousLureFishingPlayer : ModPlayer
     {
-        public override string Texture => "Fargowiltas/Content/Buffs/PlaceholderBuff";
-        public SuspiciousLookingLureBuff() : base(() => [NPCID.EyeballFlyingFish, NPCID.ZombieMerman], () => Main.bloodMoon && Main.LocalPlayer.ZoneBeach, 0.2f)
+        public override bool? CanConsumeBait(Item bait) => bait.type == ModContent.ItemType<SuspiciousLookingLure>() ? true : base.CanConsumeBait(bait);
+
+        public override void CatchFish(FishingAttempt attempt, ref int itemDrop, ref int npcSpawn, ref AdvancedPopupRequest sonar, ref Vector2 sonarPosition)
         {
+            if (Main.bloodMoon && attempt.playerFishingConditions.BaitItemType == ModContent.ItemType<SuspiciousLookingLure>())
+            {
+                npcSpawn = Main.rand.NextBool() ? NPCID.ZombieMerman : NPCID.EyeballFlyingFish;
+                itemDrop = -1;
+            }
+        }
+
+        public override void GetFishingLevel(Item fishingRod, Item bait, ref float fishingLevel)
+        {
+            if (bait.type == ModContent.ItemType<SuspiciousLookingLure>())
+            {
+                if (Main.bloodMoon)
+                    Player.displayedFishingInfo = Language.GetTextValue("GameUI.FishingWarning");
+                else fishingLevel = -1;
+            }
         }
     }
 }

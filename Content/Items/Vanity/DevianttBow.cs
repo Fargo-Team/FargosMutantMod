e841cab691f8fdcfd32debea6360d3d2f24e07c0
Expert <oofoofdlshog@gmail.com>
2026-08-31 00:42:12 +0300

    deviantt redesign vanity items

---
+++ Content/Items/Vanity/DevianttBow.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Content.Items.Vanity;
+
+[AutoloadEquip(EquipType.Back)]
+public class DevianttBow : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.width = 50;
+        Item.height = 40;
+        Item.rare = ItemRarityID.Blue;
+        Item.vanity = true;
+        Item.accessory = true;
+    }
+}
+
+public class DevianttBowDrawLayer : PlayerDrawLayer
+{
+    public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => drawInfo.shadow == 0 && drawInfo.drawPlayer.back == EquipLoader.GetEquipSlot(Mod, "DevianttBow", EquipType.Back);
+
+    public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.Wings);
+
+    protected override void Draw(ref PlayerDrawSet drawInfo)
+    {
+        Player player = drawInfo.drawPlayer;
+        Texture2D texture = ModContent.Request<Texture2D>("Fargowiltas/Content/Items/Vanity/DevianttBow_DrawLayer").Value;
+        Vector2 position = drawInfo.bodyVect + drawInfo.Position - Vector2.UnitX * (float)(drawInfo.drawPlayer.bodyFrame.Width / 2 + drawInfo.drawPlayer.width / 2);
+
+        position += new Vector2(player.direction == -1 ? 2 : -2, player.gravDir == 1 ? -6 : 3) +
+            Main.OffsetsPlayerHeadgear[player.bodyFrame.Y / player.bodyFrame.Height] * player.gravDir;
+
+        Vector2 offset = new(player.direction > 0 ? -11.5f : -player.width - 7.5f, 6);
+        position -= offset;
+
+        Color color = Lighting.GetColor(position.ToTileCoordinates());
+
+        var data = new DrawData(texture, position - Main.screenPosition, null, color, player.headRotation, texture.Size() * 0.5f, 1, drawInfo.playerEffect, 0);
+        drawInfo.DrawDataCache.Add(data);
+    }
+}
--- Content/Items/Vanity/DevianttMask.cs
+++ Content/Items/Vanity/DevianttMask.cs
@@ -1,4 +1,8 @@
-﻿using Terraria.ID;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Fargowiltas.Content.Items.Vanity;
@@ -6,6 +10,11 @@ namespace Fargowiltas.Content.Items.Vanity;
 [AutoloadEquip(EquipType.Head)]
 public class DevianttMask : ModItem
 {
+    public override void SetStaticDefaults()
+    {
+        ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
+    }
+
     public override void SetDefaults()
     {
         Item.width = 18;
@@ -21,5 +30,41 @@ public class DevianttMask : ModItem
             .AddIngredient(ItemID.MetalDetector)
             .AddTile(TileID.TinkerersWorkbench)
             .Register();
+    }
+}
+
+public class DevianttMaskDrawLayer : PlayerDrawLayer
+{
+    public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => drawInfo.shadow == 0 && drawInfo.drawPlayer.head == EquipLoader.GetEquipSlot(Mod, "DevianttMask", EquipType.Head);
+
+    public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.Head);
+    public override bool IsHeadLayer => true;
+
+    protected override void Draw(ref PlayerDrawSet drawInfo)
+    {
+        Player player = drawInfo.drawPlayer;
+        Texture2D texture = ModContent.Request<Texture2D>("Fargowiltas/Content/Items/Vanity/DevianttMask_Head_DrawLayer").Value;
+
+        // what the fuck is this
+        Vector2 position = drawInfo.helmetOffset +
+            new Vector2((int)(drawInfo.Position.X - (float)(drawInfo.drawPlayer.bodyFrame.Width / 2) +
+            (float)(drawInfo.drawPlayer.width / 2)),
+            (int)(drawInfo.Position.Y +
+            (float)drawInfo.drawPlayer.height -
+            (float)drawInfo.drawPlayer.bodyFrame.Height + 4f)) +
+            drawInfo.drawPlayer.headPosition +
+            drawInfo.headVect +
+            new Vector2(player.direction == -1 ? 2 : -2, player.gravDir == 1 ? -6 : 5) +
+            Main.OffsetsPlayerHeadgear[player.bodyFrame.Y / player.bodyFrame.Height] * player.gravDir;
+
+        position.Y += 5.5f;
+        position.X -= 2 * player.direction;
+
+        Color color = Lighting.GetColor(position.ToTileCoordinates());
+
+        var data = new DrawData(texture, position - Main.screenPosition, null, color, player.headRotation, texture.Size() * 0.5f, 1, drawInfo.playerEffect);
+        data.shader = drawInfo.cHead;
+        drawInfo.DrawDataCache.Add(data);
+        return;
     }
 }
